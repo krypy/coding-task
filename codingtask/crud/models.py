deba18d77d49b46a29e6d9d2c7ec9096f1f36a59
Cristobal J.C <cristobaljimenezcubero@gmail.com>
2018-09-10 18:40:36 +0200

    Database model

--- codingtask/crud/models.py
+++ codingtask/crud/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Administrator(AbstractUser):
+    class Meta(AbstractUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
+
+
+#
+# django User  == task Administrator
+# Client       == task User
+#
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    iban = models.CharField(max_length=34)
+    created_by = models.ForeignKey(Administrator, on_delete=models.CASCADE)
+
