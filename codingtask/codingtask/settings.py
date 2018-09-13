908188bced7f239acf1afd4a069479e1620ef2e3
Cristobal J.C <cristobaljimenezcubero@gmail.com>
2018-09-13 19:16:09 +0200

    added postgresql

--- codingtask/codingtask/settings.py
+++ codingtask/codingtask/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = 'codingtask.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coding_task',
+        'USER': 'vagrant',
+        'PASSWORD': 'vagrant-secret',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
